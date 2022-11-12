d6c3c5100cc6cd8b0f3f9a395311aecf01568159
vyacheslav.shakhov <vyacheslav.shakhov@altia.es>
2022-11-12 17:38:38 +0100

    first commit

--- src/app/a/a.component.ts
+++ src/app/a/a.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { BComponent } from '../b/b.component';
 
 @Component({
@@ -7,21 +7,10 @@ import { BComponent } from '../b/b.component';
   styleUrls: ['./a.component.css'],
 })
 export class AComponent implements AfterViewInit {
-  @ViewChild(BComponent) b!: BComponent; // access b comp by b tag
-  // @ViewChild('bRef1') b!: BComponent; // access b comp by ref
-
-  // @ViewChild(BComponent, { read: ElementRef }) b!: ElementRef; // access to <b> as DOM belonging to a (not b's html)
-  @ViewChild('header') header!: ElementRef;
+  
   constructor() {}
   ngAfterViewInit(): void {
-    console.log({ bField: this.b.x }); // access b comp fields
-    // console.log({ b: this.b });
-    console.log({ header: this.header });
-    this.header.nativeElement.style.color = 'red';
+   
   }
 }
 
-/*
-ViewChild accesses first match
-use ref if > 1 el but diff behaviour 
-*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AComponent } from './a/a.component';
 import { BComponent } from './b/b.component';
+import { CComponent } from './c/c.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AComponent,
-    BComponent
+    BComponent,
+    CComponent
   ],
   imports: [
     BrowserModule
